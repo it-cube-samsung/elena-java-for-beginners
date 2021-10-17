09df66f3b40b4bd2f85c153fbdb873533fb0d7f2
Leni <lenamalceva210@gmail.com>
2021-10-17 20:43:01 +0500

    2.3

--- src/simpletictactoe/tastBefore2_3.java
+++ src/simpletictactoe/tastBefore2_3.java
@@ -2,7 +2,7 @@ package simpletictactoe;
 
 import java.util.Scanner;
 
-class Main {
+class tastBefore2_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char [][] tikTakArr = new char[3][3];
@@ -10,6 +10,7 @@ class Main {
         String side = "|";
         String space = " ";
         char empty = '_';
+
 
         System.out.print("Enter cells: ");
         String userGame = scanner.next();
@@ -29,50 +30,27 @@ class Main {
                 num++;
                 if (j == 2) {
                     System.out.print(side);
+
                 }
             }
             System.out.println();
         }
         System.out.println(line);
-        int sumX = 0;
-        int sumY = 0;
         boolean emptyCells = false;
-        boolean winsX = false;
-        boolean winsY = false;
+        boolean threeInRow = false;
         for (int i = 0; i < tikTakArr.length; i++) {
             for (char ch : tikTakArr[i]) {
                 if (ch == '_') {
                     emptyCells = true;
                     break;
-                }
-                if (ch == 'x') {
-                    sumX++;
-                }
-                if (ch == 'y') {
-                    sumY++;
-                }
-            }
-
-            if (tikTakArr[i][0] == 'x' && tikTakArr[i][1] == 'x' && tikTakArr[i][2] == 'x' ||
-                tikTakArr[0][i] == 'x' && tikTakArr[1][i] == 'x' && tikTakArr[2][i] == 'x') {
-                winsX = true;
-            }
-            if (tikTakArr[i][0] == 'y' && tikTakArr[i][1] == 'y' && tikTakArr[i][2] == 'y' ||
-                tikTakArr[0][i] == 'y' && tikTakArr[1][i] == 'y' && tikTakArr[2][i] == 'y') {
-                winsY = true;
-            }
-        }
-        if (winsX && winsY || sumX - sumY >= 2 || sumY - sumX >= 2 ){
-            System.out.println("Impossible");
-
-        } else if(!winsX && !winsY && emptyCells){
-            System.out.println("Game not finished");
-        }else if (!winsX && !winsY && !emptyCells){
-            System.out.println("End");
-        } else if (winsX){
-            System.out.println("X wins");
-        } else if (winsY){
-            System.out.println("Y wins");
+                }}
+//            for (int j = 0; j < tikTakArr[i].length; j++) {
+//                if (tikTakArr[i][j] == '_') {
+//                    emptyCells = true;
+//                }
+//                if(tikTakArr[i][1] == 'x' && ){
+//                }
+//            }
         }
     }
 }
