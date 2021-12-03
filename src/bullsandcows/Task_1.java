5a6d6da173de0827531ee26ac49e8faf563659f5
Lenkeri <lno4kamalt@yandex.ru>
2021-12-03 19:09:49 +0500

    one

--- src/bullsandcows/Task_1.java
+++ src/bullsandcows/Task_1.java
@@ -1,7 +1,16 @@
 package bullsandcows;
 
+import java.util.Scanner;
+
 public class Task_1 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("The secret code is prepared: ");
+//        String code = scan.nextLine();
+        System.out.print(" ");
+        int bulls = 0;
+        int cows = 0;
+        String code = "9846";
     }
-        }
+}
