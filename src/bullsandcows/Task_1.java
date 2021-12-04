21970e19a1342075b893d8802b5ec3a9642b9683
Lenkeri <lenamalceva210@gmail.com>
2021-12-04 23:23:52 +0500

    быки и коровы continuation

--- src/bullsandcows/Task_1.java
+++ src/bullsandcows/Task_1.java
@@ -6,25 +6,46 @@ public class Task_1 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        System.out.print("The secret code is prepared: ****");
-//        String code = scan.nextLine();
-        System.out.print(" ");
+        System.out.println("The secret code is prepared: ****");
         int bulls = 0;
         int cows = 0;
         String code = "9846";
-        String answer = scan.nextLine();
-        cows = getCows(code, answer);
-        bulls = getBulls(code, answer);
 
-        System.out.println("you are have" + cows);
+        while (bulls < 4) {
+            int turn = 1;
+            System.out.println("Turn " + turn + ". Answer:");
+            String answer = scan.nextLine();
+            cows = getCows(code, answer);
+            bulls = getBulls(code, answer);
+
+//            if (cows > 0 && bulls > 0) {
+//                System.out.println("Grade: " + bulls + " bull and " + cows + " cow.");
+//            } else  if (bulls > 0 && cows == 0) {
+//                System.out.println("Grade: " + bulls + " bull.");
+//            }/* else  if (cows > 0 && bulls == 0) {
+//                System.out.println("Grade: " + bulls + " bull.");
+//            } */else if (cows == 0 ) {
+//                System.out.println("Grade: " + cows + " cow.");
+//            } else  if (bulls == 0) {
+//                System.out.println("Grade: " + bulls + " bull.");
+//            } else {
+//                System.out.println("Grade: None.");
+//            }
+                System.out.println("Grade: " + bulls + " bull and " + cows + " cow.");
+
+            turn++;
+        }
+        System.out.println("Congrats! The secret code is " + code + ".");
+
+
+
     }
 
     public static int getCows( String code, String answer) {
         int cow = 0;
-        for (int i = 0; i < answer.length() ; i++) {
+        for (int i = 0; i <- answer.length() ; i++) {
             for (int j = 0; j <= i; j++) {
-                if (code.charAt(i) == answer.charAt(j) && code.charAt(j) != answer.charAt(j)){
+                if (code.charAt(j) == answer.charAt(i) && code.charAt(j) != answer.charAt(j)){
                     cow++;
                 }
             }
