349eaae798c544c07f2db3f0cd7a409e77e0c3ce
Lenkeri <lenamalceva210@gmail.com>
2021-12-13 23:04:07 +0500

    Task 4 start

---
+++ src/bullsandcows/Task_4.java
@@ -0,0 +1,16 @@
+package bullsandcows;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder secretCode = new StringBuilder();
+        int lengthSecretCode = scan.nextInt();
+        Random random = new Random();
+        int bulls = 0;
+        int cows = 0;
+
+    }
+}
